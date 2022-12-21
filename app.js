78c736c0ae8dc4f6519ec79b01e48c919445e8d8
kiranmayi <kiranmayi1609@gmail.com>
2022-12-21 15:40:47 +0100

    My javascript

---
+++ app.js
@@ -0,0 +1,99 @@
+let form=document.getElementById("form");
+let textInput=document.getElementById("textInput");
+let dateInput=document.getElementById("dateInput");
+let textarea=document.getElementById("textarea");
+let msg=document.getElementById("msg");
+let tasks=document.getElementById("tasks");
+let add=document.getElementById("add");
+//call back function 
+form.addEventListener("submit",(e)=>{
+    e.preventDefault();
+    formvalidation();
+
+});
+
+let formvalidation=()=>{
+    if (textInput.value===""){
+      console.log("failure");
+      msg.innerHTML="Task cannot be blank ";
+    } else {
+        console.log("success")
+        msg.innerHTML=" ";
+        acceptData();
+        add.setAttribute("data-bs-dismiss","modal");
+        add.click();
+        
+        (()=>{
+            add.setAttribute("data-bs-dismiss","");
+        })()
+    }
+
+};
+
+let data=[]
+let acceptData=()=>{
+    data.push({
+   text:textInput.value,
+    date:dateInput.value,
+    description:textarea.value,
+    
+
+    });
+    localStorage.setItem("data", JSON.stringify(data));
+    
+    console.log(data);
+    createTasks();
+
+};
+
+let createTasks=()=> {
+    tasks.innerHTML="";
+    data.map((x,y)=>{
+        return (tasks.innerHTML+=`
+        <div id=${y}>
+          <span class="fw-bold">${x.text}</span>
+          <span class="small text-secondary">${x.date}</span>
+          <p>${x.description} </p>
+          <span class="options">
+            <i onclick="editTask(this)" data-bs-toggle="modal" data-bs-target="#form" class="fa-solid fa-pen-to-square"></i>
+            <i onclick="deleteTask(this) ;createTasks()" class="fa-solid fa-trash"></i>
+          </span>
+        </div>
+        `);
+    })
+    
+    resetfrom();
+
+};
+let deleteTask =(e)=>{
+    e.parentElement.parentElement.remove();
+    data.splice(e.parentElement.parentElement.id,1);
+    localStorage.setItem("data",JSON.stringfly(data));
+    console.log( e.parentElement.parentElement.id);
+
+};
+
+let editTask=()=>{
+   let selectedTask= e.parentElement.parentElement;
+
+    textInput.value=selectedTask.children[0].innerHTML;
+    dateInput.value=selectedTask.children[1].innerHTML;
+    textarea.value=selectedTask.children[2].innerHTML;
+   
+    deleteTask(e);
+
+
+};
+let resetfrom=()=>{
+    textInput.value="";
+    dateInput.value="";
+    textarea.value="";
+
+};
+
+(()=>{
+   data= JSON.parse(localStorage.getItem("data")) ||[];
+   createTasks();
+
+})();
+
